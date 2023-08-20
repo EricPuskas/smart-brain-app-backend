20cda9d5119d7b0fed9a9b84069bf36cfbbb5c57
Eric Puskas <eric.puskas@yahoo.com>
2023-08-20 07:37:46 +0300

    Fix cors

--- server.js
+++ server.js
@@ -19,14 +19,17 @@ mongoose.connect(process.env.DB_URI).then(() => {
 });
 
 // Use the cors middleware with appropriate options
-app.use(
-  cors({
-    origin: "*",
-    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-    preflightContinue: false,
-    optionsSuccessStatus: 204,
-  })
-);
+app.use(cors());
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin,X-Requested-With,Content-Type,Accept,content-type,application/json"
+  );
+  next();
+});
 app.use(express.json());
 
 app.post("/signin", (req, res) => {
